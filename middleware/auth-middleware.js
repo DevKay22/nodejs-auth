5d83531f7e3d724e6680bf3aed7858affcba90a1
Kayode <drkay4sure@gmail.com>
2025-07-01 14:54:50 +0100

    home route

---
+++ middleware/auth-middleware.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+
+const authMiddleware = (req, res, next) => {
+    
+
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader)
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({
+            success: false,
+            message : 'Access denied! please login'
+        })
+    }
+
+    //decode this token 
+    try {
+        const decodedTokenInfo = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log(decodedTokenInfo);
+
+        req.userInfo = decodedTokenInfo;
+        next();
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success : false,
+            message: "Something went wrong, access denied!"
+        })
+
+    }
+
+    next()
+}
+
+module.exports = authMiddleware
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const authMiddleware = require('../middleware/auth-middleware');
 const router = express.Router();
 
 
-router.get('/welcome', (req, res) => {
+router.get('/welcome', authMiddleware, (req, res) => {
+
+    const { username, userId, role} = req.userInfo;
+    
     res.json({
-        message: 'Welcome to the home page'
+        message: 'Welcome to the home page',
+         user: {
+        _Id : userId,
+        username,
+        role
+    }
     })
 })
 
