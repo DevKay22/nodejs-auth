ac8637e91f053882602509b21175cb47c226d311
Kayode <drkay4sure@gmail.com>
2025-07-03 23:18:23 +0100

    change password

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -107,6 +107,53 @@ const loginUser = async (req, res) => {
     }
 }
 
+const changePassword = async (req, res){
+    try {
+        const userId = req.userInfo.userId;
+
+        //extract old and new password
+        const {oldPassword, newPassword} = req.body;
+        
+        //find the current user logged in
+        const user = await User.findById(userId);
+
+        if(!user) {
+            return res.status(400).json({
+                success: false,
+                message: 'User not found'
+            })
+        }
+
+        //check if the old password is correct
+        const isPasswordMatch = await bcrypt.compare(oldPassword, user.password);
+
+        if(!isPasswordMatch) {
+            return res.status(400).json({
+                success: false,
+                message: 'old password is not correct. Pls try again'
+            })
+        }
+
+        //hash the new password
+        const salt = await bcrypt.genSalt(10);
+        const newHashedPassword = await bcrypt.hash(newPassword, salt)
+
+        //update user password
+        user.password = newHashedPassword;
+        await user.save();
+
+        return res.status(200).json({
+            success: true,
+            message: 'Password changed successfully'
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong, Please try again'
+        })
+    }
+}
 
 module.exports = {
     registerUser,
